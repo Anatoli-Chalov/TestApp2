29f2842f0923dcf1199572bac2ad82bcdb07fd83
Anatoli-Chalov <chalovad@bk.ru>
2025-02-07 12:53:42 +0300

    Update Program.cs

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -1,11 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TestApp
-
-    {
-       WriteLine("Hello World!");
-    }
+﻿using System; 
+ using System.Collections.Generic; 
+ using System.Linq; 
+ using System.Text; 
+ using System.Threading.Tasks; 
+  
+ namespace TestApp 
+ { 
+     internal class Program 
+     { 
+         static void Main(string[] args) 
+         { 
+         } 
+     } 
+ }
