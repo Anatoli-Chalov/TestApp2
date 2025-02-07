7b73138e7e796cb093e5d8ea01c8f04f4e903b47
Anatoli-Chalov <chalovad@bk.ru>
2025-02-07 12:54:03 +0300

    Update Program.cs

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -1,4 +1,4 @@
-﻿using System; 
+ ﻿using System; 
  using System.Collections.Generic; 
  using System.Linq; 
  using System.Text; 
@@ -8,7 +8,7 @@
  { 
      internal class Program 
      { 
-         static void Main(string[] args) 
+       static void Main(string[] args) 
          { 
          } 
      } 
